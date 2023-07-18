a4adc3d8dfa11ac7e6625ea3a3ca2c3ff9aadb16
Hunter Andersen <andersen.hunter42@gmail.com>
2023-07-18 09:33:02 -0700

    Converted from class to functional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,136 +1,101 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import PreviousGuess from './PreviousGuess';
 import './App.css';
 
 const API_KEY = `dDiB2TbsVlgYtU3itSJwjhR5dM6PUZXj`;
 
 //Inheriting from the react library
-class App extends Component{
-
-  //Building our App - intial setup
-  constructor(){
-    super();
-
-    //Create (initialize) the state
-    this.state = {
-      randomNumber: Math.floor(Math.random() * 100),
-      inputGuess: "",
-      feedbackMessage: "Guess a number between 1 and 100",
-      imgUrl: "",
-      prevGuesses: []
+function App() {
+  const [feedbackMessage, setFeedbackMessage] = useState("Guess a number between 1 and 100");
+  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * 100));
+  const [prevGuesses, setPrevGuesses] = useState([]);
+  const [inputGuess, setInputGuess] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  
+  //Empty dependency array will make useEffect run only once
+  useEffect(() => {
+    const controller = new AbortController();
+    getGif(feedbackMessage, controller);
+    return () => {
+      //Performs clean up operations
+      if (process.env.NODE_ENV !== "development") {
+        controller.abort();
+      }
     }
+  }, [feedbackMessage]);
 
-    //Fetch abort controller
-    this.controller = new AbortController();
-
-    //Keep our methods from getting lost
-    //So they'll always know what "this" refers to
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  componentDidMount(){
-    this.getGif("Welcome to the game show");
-  }
-
-  getGif(searchTerm){
+  function getGif(searchTerm, controller){
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${searchTerm}&weirdness=0`, 
     {
-      signal: this.controller.signal
+      signal: controller.signal
     })
     .then((response) => {
       return response.json();
     })
     .then((result) => {
       console.log(result);
-      this.setState({
-        imgUrl: result.data.images.fixed_height.url
-      });
+      setImgUrl(result.data.images.fixed_height.url);
     })
     .catch((err) => {
       console.error(err);
     })
   }
 
-  componentWillUnmount(){
-    if (process.env.NODE_ENV !== "development") {
-      this.controller.abort();
-    }
-  }
-
-  //A method that handles the submit event on the form
-  handleSubmit(ev) {
+  //A function that handles the submit event on the form
+  function handleSubmit(ev) {
     ev.preventDefault();
     //Converts from a string to a number
-    const tempGuess = parseInt(this.state.inputGuess);
+    const tempGuess = parseInt(inputGuess);
     //Track the guess as a previous guess
-    this.setState({
-      prevGuesses: [...this.state.prevGuesses, tempGuess]
-    });
+    setPrevGuesses([...prevGuesses, tempGuess]);
     //Perform the logic to determine higher/lower
-    if (this.state.randomNumber > tempGuess){
+    if (randomNumber > tempGuess){
       //Make the DOM say "higher"
-      this.setState({
-        feedbackMessage: "Higher..."
-      });
-      this.getGif(`Guess a number`);
-    } else if (this.state.randomNumber < tempGuess){
+      setFeedbackMessage("Higher...");
+    } else if (randomNumber < tempGuess){
       //Make the DOM say "lower"
-      this.setState({
-        feedbackMessage: "Lower..."
-      });
-      this.getGif(`Guess a number`);
+      setFeedbackMessage("Lower...");
     } else {
       //Make the DOM say "You win!"
-      this.setState({
-        feedbackMessage: "You win!"
-      });
-      this.getGif("You won!");
+      setFeedbackMessage("Winner, winner!");
     }
     //All the setState calls are updating our state to a new thing
     //so that we have a perfect knowledge of everything our app
     //needs to track
   }
 
-  //A method that handles the change event on the input
-  handleChange(ev) {
+  //A function that handles the change event on the input
+  function handleChange(ev) {
     //Update my state, because the user just typed something
     //and I want my state to know what got typed!
-    this.setState({
-      inputGuess: ev.target.value
-    });
+    setInputGuess(ev.target.value);
   }
 
-  //Overriding the render method from the react Component class
-  //It causes things to get displayed on the screen
-  render() {
-    //returns JSX
-    return (
-      <>
-        <h1>Guessing Game</h1>
-        <p>{this.state.feedbackMessage}</p>
-        <form className="guessForm" onSubmit={this.handleSubmit}>
-          <label htmlFor="inputGuess">Guess a number</label>
-          {/* Needs to be a controlled input */}
-          <input 
-            type="text" 
-            id="inputGuess" 
-            value={this.state.inputGuess} 
-            onChange={this.handleChange}
-          />
-          <button type="submit">Guess</button>
-        </form>
-        <img 
-          src={this.state.imgUrl} 
-          alt="Giphy Gif" 
+  //returns JSX
+  return (
+    <>
+      <h1>Guessing Game</h1>
+      <p>{feedbackMessage}</p>
+      <form className="guessForm" onSubmit={handleSubmit}>
+        <label htmlFor="inputGuess">Guess a number</label>
+        <input 
+          type="text" 
+          id="inputGuess" 
+          value={inputGuess} 
+          onChange={handleChange}
         />
-        <PreviousGuess 
-          guesses={this.state.prevGuesses} 
-          favColor="green"
-        />
-      </>
-    );
-  }
+        <button type="submit">Guess</button>
+      </form>
+      <img 
+        src={imgUrl} 
+        alt="Giphy Gif" 
+      />
+      <PreviousGuess 
+        guesses={prevGuesses} 
+        favColor="green"
+      />
+    </>
+  );
 }
-
+ 
 export default App;
